157c3e7a70df4995005de5f8dbf994a97894e281
Macintosh <macintosh@lap0891.lan>
2023-06-11 14:01:03 +0200

    removed example ui

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -46,12 +46,6 @@ export const Header = () => {
         </NavLink>
       </li>
       <li>
-        <NavLink href="/example-ui">
-          <SparklesIcon className="h-4 w-4" />
-          Example UI
-        </NavLink>
-      </li>
-      <li>
         <NavLink href="/blockexplorer">
           <MagnifyingGlassIcon className="h-4 w-4" />
           Block Explorer
