ceae6427163f4679ce741477087a66b77b6c3f27
Macintosh <macintosh@lap0891.lan>
2023-06-12 11:30:35 +0200

    added project title and description

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -84,8 +84,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">theBridge</span>
+            <span className="text-xs">Bridge tokens between <br />layer-1 and layer-2</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
