9579e60a1eac914bf441f8c3ea854da90494b6a2
Macintosh <macintosh@lap0891.lan>
2023-06-12 11:27:32 +0200

    hide balance if balance has not been retrieved

--- packages/nextjs/components/forms/WithdrawForm.tsx
+++ packages/nextjs/components/forms/WithdrawForm.tsx
@@ -140,7 +140,7 @@ function WithdrawForm({}: Props) {
             </div>
           </NumberInput>
         </div>
-        <p className='text-right text-sm text-gray-700'>Balance: {balance}</p>
+        {balance !== null && <p className='text-right text-sm text-gray-700'>Balance: {balance}</p>}
 
         <Button label='Withdraw' className='w-full' onClick={withdraw} isLoading={isWithdrawing} />
       </>
